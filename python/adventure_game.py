eeca7d0c3497ec06e71c5d9c3430a9ba63154ab3
Brian Segers <briansegs@gmail.com>
2020-10-09 11:31:41 -0400

    Add fight system prototype

--- python/adventure_game.py
+++ python/adventure_game.py
@@ -7,13 +7,13 @@ def coin_flip():
 def die_roll():
 	return random.randint(1, 6)
 
-def t_print(string):
+def time_print(string):
     print(string)
     time.sleep(1)
 
-def t_print_loop(lst):
+def time_print_loop(lst):
     for i in range(len(lst)):
-        t_print(lst[i])
+        time_print(lst[i])
 
 def valid_input(promt, option1, option2):
     while True:
@@ -23,63 +23,90 @@ def valid_input(promt, option1, option2):
         if option2 in response:
             return option2
         else:
-            t_print("Sorry, I don't understand.")
+            time_print("Sorry, I don't understand.")
     return response
 
-def player_attack(hp):    
-    dmg = (die_roll() + die_roll())
-    hp['player'] -= dmg
-    lst = ['The Boss unsheaths his sword, darts twards you, and slashes you with a swift blow.',
-    f'Boss hits you for {dmg} damage points.', 
-    f'your health is now at {hp["player"]}'
-    ]
-    t_print_loop(lst)
-    if hp['player'] > 0:
-        answer = valid_input('Continue fighting or flee the battle? Type flee or fight.', 'fight', 'flee')
+def boss_attack(stats):    
+    if stats['boss_name'] == 'Big Boi':
+        dmg = die_roll() + die_roll() 
+    else:
+        dmg = die_roll() * 2 
+    stats['player_hp'] -= dmg
+    lst = [
+        f"{stats['boss_name']} unsheaths his sword, darts twards you, and slashes you with a swift blow.",
+        f"{stats['boss_name']} hits you for {dmg} damage points.", 
+        f"your health is now at {stats['player_hp']}"
+        ]
+    time_print_loop(lst)
+    if stats['player_hp'] > 0:
+        answer = valid_input('Continue fighting or flee the battle? Type flee or fight.\n', 'fight', 'flee')
         if answer == "fight":
-            t_print('Player- "I will not back down!"')
-            who_attacks(hp)
+            time_print(f'''{stats['player_name']} - "I will not back down!"''')
+            who_attacks(stats)
         elif answer == "flee":
-            lst = ['Player- "Im in over my head."', 
-            'You run from the battle and escape Boss.', 
-            'You live to fight another day.'
-            ]
-            
-            
+            lst = [
+                f'''{stats['player_name']} - "Im in over my head."''', 
+                f"{stats['player_name']} run from the battle and escape Boss.", 
+                "You live to fight another day."
+                ]
+            time_print_loop(lst)
     else:
-        t_print('You have died')
+        time_print('You have died')
         
-
-
-def boss_attack(hp):
-    dmg = (die_roll() * 2)
-    hp['boss'] -= dmg
-    t_print('You bolt toward Boss, lifting oath breaker above your head, and then strick Boss with a heavy blow')
-    t_print(f'You hit Boss for {dmg} damage points.')
-    t_print(f"Boss's health is now at {hp['boss']}")
-    if hp['boss'] > 0:
-        t_print('Boss- "Not bad!"')
-        who_attacks(hp)
+def player_attack(stats):
+    if stats['boss_name'] == 'Big Boi':
+        dmg = die_roll() * 2
     else:
-        t_print('You have Won!')
+        dmg = die_roll() + die_roll() 
+    stats['boss_hp'] -= dmg
+    lst = [
+        f"{stats['player_name']} bolts toward {stats['boss_name']}, lifting oath breaker above his head, and then stricks {stats['boss_name']} with a heavy blow",
+        f"You hit {stats['boss_name']} for {dmg} damage points.",
+        f"{stats['boss_name']}'s health is now at {stats['boss_hp']}"
+        ]
+    time_print_loop(lst)
+    if stats['boss_hp'] > 0:
+        time_print(f'''{stats['boss_name']} - "Not bad!"''')
+        who_attacks(stats)
+    else:
+        time_print('You have Won!')
         
-
-def who_attacks(hp):    
+def who_attacks(stats):    
     result = coin_flip()
     if result == 'heads':
-        player_attack(hp)
+        player_attack(stats)
     elif result == 'tails':
-        boss_attack(hp)
+        boss_attack(stats)
     else:
-        who_attacks(hp)
+        who_attacks(stats)
 
 def intro():    
-    t_print('You are both still, waiting for the right time to make your move, and then...')
+    time_print('You are both still, waiting for the right time to make your move, and then...')
 
+def choose_stats(items = {'key':'thing1', 'player_name':'Brian'}):
+    opt1 = {
+        'boss_hp': 10, 
+        'player_hp': 10,
+        'boss_name': 'Big Boi',
+        'player_name':'None'
+        }
+    opt2 = {
+        'boss_hp': 10, 
+        'player_hp': 10, 
+        'boss_name': 'Small Boi',
+        'player_name':'None'
+        }
+    if items['key'] == 'thing1':
+        opt1['player_name'] = items['player_name']
+        stats = opt1
+    else:
+        opt2['player_name'] = items['player_name']
+        stats = opt2
+    return stats
+     
 def fight():
-    hp = {'boss': 10, 'player': 10}
     intro()
-    who_attacks(hp)
+    who_attacks(choose_stats())
 
 
 fight()
