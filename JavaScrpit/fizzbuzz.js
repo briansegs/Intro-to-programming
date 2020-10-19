7160ad94a0cd77240fbf3d579eeee42931bad128
Brian Segers <briansegs@gmail.com>
2020-10-18 23:26:04 -0400

    Add fizzbuzz

---
+++ JavaScrpit/fizzbuzz.js
@@ -0,0 +1,12 @@
+
+var x = 1;
+
+while (x <= 20) {
+   x % 3 === 0 ? (x % 5 === 0 ?
+   console.log("JuliaJames"):
+   console.log("Julia")) :
+   (x % 5 === 0 ?
+   console.log("James") :
+   console.log(x));
+   x += 1;
+}
