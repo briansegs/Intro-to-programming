f96b2f849aca249ef2e354d8bca52662c75f35d2
Brian Segers <briansegs@gmail.com>
2020-10-21 16:28:55 -0400

    Add grid completed

--- JavaScrpit/project-pixel-art-maker-starter-master/designs.js
+++ JavaScrpit/project-pixel-art-maker-starter-master/designs.js
@@ -1,39 +1,32 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
-const r = 20;
-const c = 20;
-
-const tBody = document.createElement('tbody');
-document.querySelector('#pixelCanvas').appendChild(tBody);
-for (let i = 0; i < r; i++) {
-    const tr = document.createElement('tr');
-    // tr.setAttribute('class', 'test');
-    tBody.appendChild(tr)
-    for (let y = 0; y < c; y++) {
-        const td = tr.appendChild(document.createElement('td'));
-        td.addEventListener('click', function () {
-            console.log('Click!');
-            let color = document.getElementById("colorPicker").value;
-            td.setAttribute('style', "background : " + color);
-        });
+function makeGrid(row, col) {
+    const tBody = document.createElement('tbody');
+    document.querySelector('#pixelCanvas').appendChild(tBody);
+    for (let i = 0; i < row; i++) {
+        const tr = document.createElement('tr');
+        tBody.appendChild(tr)
+        for (let y = 0; y < col; y++) {
+            const td = tr.appendChild(document.createElement('td'));
+            td.addEventListener('click', function () {
+                let color = document.getElementById("colorPicker").value;
+                td.setAttribute('style', "background : " + color);
+            });
+        }
     }
-}
+};
 
 
-function makeRow() {
-    const tBody = document.querySelector('tbody');
-    var tr = document.createElement('tr');
-    var td = document.createElement('td');
-    tr.setAttribute('class', 'test');
-    tr.appendChild(td);
-    tBody.appendChild(tr)
-}
+
+document.addEventListener('submit', function (event) {
+    const row = document.getElementById("inputHeight").value;
+    const col = document.getElementById("inputWidth").value;
+    document.querySelector('#pixelCanvas').innerHTML = '';
+    makeGrid(row, col);
+    event.preventDefault();
+});
+
+
+makeGrid(10, 10);
+
 
