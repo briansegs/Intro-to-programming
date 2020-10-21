d6fbfee084d0e4ecc07a6822293175203221ef96
Brian Segers <briansegs@gmail.com>
2020-10-21 14:57:28 -0400

    Add starter files and current work

---
+++ JavaScrpit/project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,37 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
+const r = 20;
+const c = 20;
+
+const tBody = document.createElement('tbody');
+document.querySelector('#pixelCanvas').appendChild(tBody);
+for (let i = 0; i < r; i++) {
+    const tr = document.createElement('tr');
+    // tr.setAttribute('class', 'test');
+    tBody.appendChild(tr)
+    for (let y = 0; y < c; y++) {
+        const td = tr.appendChild(document.createElement('td'));
+        td.addEventListener('click', function () {
+            console.log('Click!')
+        });
+    }
+}
+
+
+function makeRow() {
+    const tBody = document.querySelector('tbody');
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    tr.setAttribute('class', 'test');
+    tr.appendChild(td);
+    tBody.appendChild(tr)
+}
+
